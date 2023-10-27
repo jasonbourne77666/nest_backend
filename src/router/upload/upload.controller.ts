cbe65197f6223c21e568f2e41ae9e931e44ddb50
Jason <jason@gmail.com>
2023-10-27 22:38:28 +0800

    feat: 大文件上传

--- src/router/upload/upload.controller.ts
+++ src/router/upload/upload.controller.ts
@@ -1,17 +1,23 @@
 import {
   Controller,
   Post,
+  HttpException,
+  HttpStatus,
   UploadedFile,
   UploadedFiles,
   UseInterceptors,
   Get,
   Res,
+  Req,
   Query,
   BadRequestException,
   ParseIntPipe,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import * as sharp from 'sharp';
+import * as path from 'path';
+import formidable from 'formidable';
+import * as concat from 'concat-files';
 
 import {
   FilesInterceptor,
@@ -19,6 +25,9 @@ import {
 } from '@nestjs/platform-express';
 import { existsSync } from 'fs';
 import { UploadService } from './upload.service';
+import { listDir, isExist, folderIsExit, copyFile } from '../../utils';
+
+const uploadDir = 'uploads';
 
 @Controller('upload')
 export class UploadController {
@@ -41,6 +50,7 @@ export class UploadController {
     return res;
   }
 
+  // 压缩
   @Get('compression')
   async compression(
     @Query('path') filePath: string,
@@ -63,5 +73,113 @@ export class UploadController {
     res.set('Content-Disposition', `attachment; filename="filename.jpg"`);
 
     res.send(data);
+  }
+
+  // 大文件
+  @Get('/check/file')
+  async checkFile(
+    @Query('fileName') fileName: string,
+    @Query('fileMd5Value') fileMd5Value: string,
+  ) {
+    const res = await this.getChunkList(
+      path.join(uploadDir, fileName),
+      path.join(uploadDir, fileMd5Value),
+    );
+
+    return res;
+  }
+
+  // 获取文件Chunk列表
+  async getChunkList(filePath: string, folderPath: string) {
+    const isFileExit = await isExist(filePath);
+    let result = {};
+    // 如果文件已在存在, 不用再继续上传, 真接秒传
+    if (isFileExit) {
+      result = {
+        stat: 1,
+        file: {
+          isExist: true,
+          name: filePath,
+        },
+        desc: 'file is exist',
+      };
+    } else {
+      const isFolderExist = await isExist(folderPath);
+      // 如果文件夹(md5值后的文件)存在, 就获取已经上传的块
+      let fileList: any[] = [];
+      if (isFolderExist) {
+        fileList = await listDir(folderPath);
+      }
+      result = {
+        stat: 1,
+        chunkList: fileList,
+        desc: 'folder list',
+      };
+    }
+    return result;
+  }
+
+  @Post('/fileSlice')
+  async fileSliceUpload(@Req() req: Request, @Res() res: Response) {
+    const form = formidable({
+      uploadDir: 'uploads/tmp',
+    });
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.log(err);
+        throw new HttpException(err.toString(), HttpStatus.BAD_REQUEST);
+      }
+      const index = fields.index;
+      const fileMd5Value = fields.fileMd5Value[0];
+      const folder = path.resolve(__dirname, '../../../uploads', fileMd5Value);
+      // 使用Md5查看文件是否存在
+      const isExistFolder = await folderIsExit(folder);
+      if (isExistFolder) {
+        const destFile = path.resolve(folder, fields.index[0]);
+        copyFile(files.data[0].filepath, destFile).then(
+          (successLog) => {
+            res.send({
+              stat: 1,
+              desc: index,
+              msg: '上传成功',
+            });
+          },
+          (errorLog) => {
+            res.send({
+              stat: 0,
+              desc: 'Error',
+              msg: errorLog,
+            });
+          },
+        );
+      }
+    });
+  }
+
+  // 合并文件
+  async mergeFiles(srcDir, targetDir, newFileName) {
+    const fileArr = await listDir(srcDir);
+    fileArr.sort((x, y) => {
+      return x - y;
+    });
+    // 把文件名加上文件夹的前缀
+    for (let i = 0; i < fileArr.length; i++) {
+      fileArr[i] = srcDir + '/' + fileArr[i];
+    }
+    concat(fileArr, path.join(targetDir, newFileName), (err) => {
+      if (err) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  @Get('/merge')
+  mergeBigFile(@Query('md5') md5: string, @Query('fileName') fileName: string) {
+    const res = this.mergeFiles(path.join(uploadDir, md5), uploadDir, fileName);
+    return {
+      stat: res ? 1 : 0,
+    };
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import * as crypto from 'crypto';
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import { ParseIntPipe, BadRequestException } from '@nestjs/common';
 
 export function md5(str) {
@@ -53,4 +55,56 @@ export function getUserDetailByUid(
     pub: pub,
   });
   return res;
+}
+
+// 列出文件夹下所有文件
+export function listDir(path: string): Promise<any[]> {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      // 把mac系统下的临时文件去掉
+      if (data && data.length > 0 && data[0] === '.DS_Store') {
+        data.splice(0, 1);
+      }
+      resolve(data);
+    });
+  });
+}
+
+// 文件或文件夹是否存在
+export function isExist(filePath: string) {
+  return new Promise((resolve, reject) => {
+    fs.stat(filePath, (err, stats) => {
+      // 文件不存在
+      if (err && err.code === 'ENOENT') {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+// 文件夹是否存在, 不存在则创建文件夹
+export function folderIsExit(folder) {
+  return new Promise(async (resolve, reject) => {
+    fs.ensureDirSync(path.join(folder));
+    resolve(true);
+  });
+}
+
+// 把文件从一个目录拷贝到别一个目录
+export function copyFile(src: fs.PathLike, dest: fs.PathLike) {
+  return new Promise((resolve, reject) => {
+    fs.rename(src, dest, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('copy file:' + dest + ' success!');
+      }
+    });
+  });
 }
